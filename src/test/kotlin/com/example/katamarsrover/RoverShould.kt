d4182139648a06edb0cfa1811ffb8ff1085519ce
juanantonioqs <juanantonio19344@hotmail.com>
2024-01-09 11:31:35 -0500

    :white_check_mark: test: start facing to the north in a specific position

--- src/test/kotlin/com/example/katamarsrover/RoverShould.kt
+++ src/test/kotlin/com/example/katamarsrover/RoverShould.kt
@@ -6,7 +6,36 @@ import org.junit.jupiter.api.Test
 class RoverShould {
 
     @Test
-    fun `xx`(){
-        Assertions.assertEquals("", "")
+    fun `start facing to the north in a specific position`(){
+        var board: Board = Board(10, 10)
+        var position: Position = Position(0, 0)
+        var direction: Direction = Direction("N")
+        var rover: Rover = Rover(board, position, direction)
+
+        Assertions.assertEquals(rover.currentPosition(), Position(0, 0))
+        Assertions.assertEquals(rover.currentDirection(), Direction("N"))
     }
 }
+
+class Rover(var board: Board, var position: Position, var direction: Direction) {
+
+    fun currentPosition(): Position{
+        return position
+    }
+
+    fun currentDirection(): Direction {
+        return direction
+    }
+
+}
+
+class Board(var height: Int, var width: Int) {}
+
+data class Position(var x: Int, var y: Int) {
+}
+
+data class Direction (var direction: String){}
+
+
+
+
