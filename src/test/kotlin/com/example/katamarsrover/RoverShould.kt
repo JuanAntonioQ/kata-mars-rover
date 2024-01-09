c8e22841bb9a2bde670a87400291415826c2a257
juanantonioqs <juanantonio19344@hotmail.com>
2024-01-09 13:23:07 -0500

    :white_check_mark: test: start facing to the north and rotate once to the right

--- src/test/kotlin/com/example/katamarsrover/RoverShould.kt
+++ src/test/kotlin/com/example/katamarsrover/RoverShould.kt
@@ -15,6 +15,16 @@ class RoverShould {
         Assertions.assertEquals(rover.currentPosition(), Position(0, 0))
         Assertions.assertEquals(rover.currentDirection(), Direction("N"))
     }
+
+    @Test
+    fun `start facing to the north and rotate once to the right`() {
+        var board: Board = Board(10, 10)
+        var position: Position = Position(0, 0)
+        var direction: Direction = Direction("N")
+        var rover: Rover = Rover(board, position, direction)
+
+        Assertions.assertEquals("0:0:E", rover.execute("R"))
+    }
 }
 
 class Rover(var board: Board, var position: Position, var direction: Direction) {
@@ -27,6 +37,15 @@ class Rover(var board: Board, var position: Position, var direction: Direction) 
         return direction
     }
 
+    fun execute(commands: String): String{
+        for (command in commands) {
+            if (command == 'R') {
+                direction = direction.turnRight()
+            }
+        }
+        return currentPosition().x.toString() + ":" + currentPosition().y.toString() + ":" + direction.value
+    }
+
 }
 
 class Board(var height: Int, var width: Int) {}
@@ -34,7 +53,20 @@ class Board(var height: Int, var width: Int) {}
 data class Position(var x: Int, var y: Int) {
 }
 
-data class Direction (var direction: String){}
+data class Direction (var value: String){
+
+    val directions = mapOf(
+        "N" to "N,W,E",
+        "E" to "E,N,S",
+        "S" to "S,E,W",
+        "W" to "W,S,N"
+    )
+    fun turnRight(): Direction {
+        var right: String = directions.get(this.value)!!.split(",")[2]
+        return Direction(right)
+    }
+
+}
 
 
 
