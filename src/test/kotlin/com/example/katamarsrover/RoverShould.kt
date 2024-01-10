bf6bb4936db58a92216414de02044e31ddbe0cfb
juanantonioqs <juanantonio19344@hotmail.com>
2024-01-09 20:40:22 -0500

    :white_check_mark: test: move from far left to far right on the board when facing to the west and it reaches the board boundary

--- src/test/kotlin/com/example/katamarsrover/RoverShould.kt
+++ src/test/kotlin/com/example/katamarsrover/RoverShould.kt
@@ -188,6 +188,16 @@ class RoverShould {
         Assertions.assertEquals("0:0:W", rover.execute("LMM"))
     }
 
+    @Test
+    fun `move from far left to far right on the board when facing to the west and it reaches the board boundary`() {
+        var board: Board = Board(10, 10)
+        var position: Position = Position(0, 0)
+        var direction: Direction = Direction("N")
+        var rover: Rover = Rover(board, position, direction)
+
+        Assertions.assertEquals("9:0:W", rover.execute("LM"))
+    }
+
 }
 
 class Rover(var board: Board, var position: Position, var direction: Direction) {
